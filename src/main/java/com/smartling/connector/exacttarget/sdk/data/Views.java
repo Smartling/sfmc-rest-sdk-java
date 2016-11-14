b0217c6af8091de6bef70211c44a86ade0cfdfe4
Mykhailo Shchukin <mshchukin@smartling.com>
2016-11-14 20:10:47 +0200

    fix public variables

--- src/main/java/com/smartling/connector/exacttarget/sdk/data/Views.java
+++ src/main/java/com/smartling/connector/exacttarget/sdk/data/Views.java
@@ -2,12 +2,92 @@ package com.smartling.connector.exacttarget.sdk.data;
 
 public class Views
 {
-    public Subjectline subjectline;
-    public Object preheader;
-    public Html html;
-    public Object text;
-    public Object viewAsAWebPage;
-    public Object subscriptioncenter;
-    public Object forwardHTML;
-    public Object forwardText;
+    private Subjectline subjectline;
+    private Object preheader;
+    private Html html;
+    private Object text;
+    private Object viewAsAWebPage;
+    private Object subscriptioncenter;
+    private Object forwardHTML;
+    private Object forwardText;
+
+    public Subjectline getSubjectline()
+    {
+        return subjectline;
+    }
+
+    public void setSubjectline(final Subjectline subjectline)
+    {
+        this.subjectline = subjectline;
+    }
+
+    public Object getPreheader()
+    {
+        return preheader;
+    }
+
+    public void setPreheader(final Object preheader)
+    {
+        this.preheader = preheader;
+    }
+
+    public Html getHtml()
+    {
+        return html;
+    }
+
+    public void setHtml(final Html html)
+    {
+        this.html = html;
+    }
+
+    public Object getText()
+    {
+        return text;
+    }
+
+    public void setText(final Object text)
+    {
+        this.text = text;
+    }
+
+    public Object getViewAsAWebPage()
+    {
+        return viewAsAWebPage;
+    }
+
+    public void setViewAsAWebPage(final Object viewAsAWebPage)
+    {
+        this.viewAsAWebPage = viewAsAWebPage;
+    }
+
+    public Object getSubscriptioncenter()
+    {
+        return subscriptioncenter;
+    }
+
+    public void setSubscriptioncenter(final Object subscriptioncenter)
+    {
+        this.subscriptioncenter = subscriptioncenter;
+    }
+
+    public Object getForwardHTML()
+    {
+        return forwardHTML;
+    }
+
+    public void setForwardHTML(final Object forwardHTML)
+    {
+        this.forwardHTML = forwardHTML;
+    }
+
+    public Object getForwardText()
+    {
+        return forwardText;
+    }
+
+    public void setForwardText(final Object forwardText)
+    {
+        this.forwardText = forwardText;
+    }
 }
