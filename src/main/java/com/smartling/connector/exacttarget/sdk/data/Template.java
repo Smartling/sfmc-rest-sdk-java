537e68be4cabd4140fbad09be2f0e6fb7e7a67ad
Mykhailo Shchukin <mshchukin@smartling.com>
2018-01-23 00:38:33 +0200

    add fields to Template

--- src/main/java/com/smartling/connector/exacttarget/sdk/data/Template.java
+++ src/main/java/com/smartling/connector/exacttarget/sdk/data/Template.java
@@ -1,7 +1,5 @@
 package com.smartling.connector.exacttarget.sdk.data;
 
-import com.smartling.connector.exacttarget.sdk.data.AssetType;
-
 public class Template
 {
     private AssetType assetType;
@@ -9,6 +7,15 @@ public class Template
     private String content;
     private Object meta;
     private Object slots;
+    private Object options;
+    private String customerKey;
+    private String objectID;
+    private Object owner;
+    private String enterpriseId;
+    private String memberId;
+    private Object status;
+    private String thumbnailUrl;
+    private String category;
 
     public AssetType getAssetType()
     {
@@ -58,5 +65,95 @@ public class Template
     public void setSlots(final Object slots)
     {
         this.slots = slots;
+    }
+
+    public Object getOptions()
+    {
+        return options;
+    }
+
+    public void setOptions(final Object options)
+    {
+        this.options = options;
+    }
+
+    public String getCustomerKey()
+    {
+        return customerKey;
+    }
+
+    public void setCustomerKey(final String customerKey)
+    {
+        this.customerKey = customerKey;
+    }
+
+    public String getObjectID()
+    {
+        return objectID;
+    }
+
+    public void setObjectID(final String objectID)
+    {
+        this.objectID = objectID;
+    }
+
+    public Object getOwner()
+    {
+        return owner;
+    }
+
+    public void setOwner(final Object owner)
+    {
+        this.owner = owner;
+    }
+
+    public String getEnterpriseId()
+    {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(final String enterpriseId)
+    {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public String getMemberId()
+    {
+        return memberId;
+    }
+
+    public void setMemberId(final String memberId)
+    {
+        this.memberId = memberId;
+    }
+
+    public Object getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(final Object status)
+    {
+        this.status = status;
+    }
+
+    public String getThumbnailUrl()
+    {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(final String thumbnailUrl)
+    {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(final String category)
+    {
+        this.category = category;
     }
 }
