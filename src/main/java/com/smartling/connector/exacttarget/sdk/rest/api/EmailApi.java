c86477033c71d17d48fdf9d8eb204cb46c3b23ea
ivanskrypka <iskrypka@smartling.com>
2019-05-15 12:59:31 +0300

    SMC-93 add sharing properties to email

--- src/main/java/com/smartling/connector/exacttarget/sdk/rest/api/EmailApi.java
+++ src/main/java/com/smartling/connector/exacttarget/sdk/rest/api/EmailApi.java
@@ -13,20 +13,20 @@ public interface EmailApi
     @RequestLine("POST /asset/v1/content/assets/query/")
     @Headers ("Content-Type: application/json") Elements<Email> listEmails(GetListRequest body);
 
-    @RequestLine("GET /asset/v1/assets/{id}")
+    @RequestLine("GET /asset/v1/content/assets/{id}")
     Email getEmail(@Param("id") String id);
 
     @RequestLine("GET /asset/v1/content/assets/{id}/channelviews/html")
     Preview getEmailPreview(@Param("id") String id);
 
-    @RequestLine("POST /asset/v1/assets/")
+    @RequestLine("POST /asset/v1/content/assets")
     @Headers ("Content-Type: application/json")
     Email createEmail(Email clonedEmail);
 
-    @RequestLine("PUT /asset/v1/assets/{id}")
+    @RequestLine("PUT /asset/v1/content/assets/{id}")
     @Headers ("Content-Type: application/json")
     Void updateEmail(@Param("id") String id, Email clonedEmail);
 
-    @RequestLine("DELETE /asset/v1/assets/{id}")
+    @RequestLine("DELETE /asset/v1/content/assets/{id}")
     String deleteEmail(@Param("id") String id);
 }
