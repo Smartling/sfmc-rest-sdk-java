b73afc6cd965baedced5205563903e5131f70a1e
ivanskrypka <iskrypka@smartling.com>
2019-12-16 12:35:14 +0200

    SMC-105 Text version is not preserved when creating/updating a language copy

--- src/main/java/com/smartling/connector/exacttarget/sdk/data/Text.java
+++ src/main/java/com/smartling/connector/exacttarget/sdk/data/Text.java
@@ -5,6 +5,8 @@ public class Text
     private Object options;
     private String content;
     private Integer modelVersion;
+    private Object data;
+    private Object generateFrom;
 
     public Object getOptions()
     {
@@ -34,5 +36,25 @@ public class Text
     public void setModelVersion(final Integer modelVersion)
     {
         this.modelVersion = modelVersion;
+    }
+
+    public Object getData()
+    {
+        return data;
+    }
+
+    public void setData(Object data)
+    {
+        this.data = data;
+    }
+
+    public Object getGenerateFrom()
+    {
+        return generateFrom;
+    }
+
+    public void setGenerateFrom(Object generateFrom)
+    {
+        this.generateFrom = generateFrom;
     }
 }
